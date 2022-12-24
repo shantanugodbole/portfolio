9e5eb104e63e15e73c1c9dcbf3ae4ead5014a860
shantanugodbole <shantanugodbole2000@gmail.com>
2022-12-24 16:31:58 +0530

    basic landing page developed

--- src/components/content/AboutMe.jsx
+++ src/components/content/AboutMe.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 
-function AboutMe(){
+function AboutMe() {
+    var today = new Date();
+    var curHr = today.getHours();
+    var displayMessage = "";
+    if (curHr < 12) {
+        displayMessage = "Good Morning";
+    }
+    else if (curHr < 17) {
+        displayMessage = "Good Afternoon";
+    }
+    else {
+        displayMessage = "Good Evening";
+    }
+
     return (
-        <div className="bg-neutral-900 h-screen w-screen ">
+        <div className="bg-neutral-900 h-screen w-screen">
             <div className="h-1/5 bg-gradient-to-b from-green-800 to-neutral-900">
-                <p className="text-white">Good Afternoon!</p>
+                <div className="text-white text-3xl font-bold py-20 px-20">
+                    <p>{displayMessage}</p>
+                </div>
+            </div>
+            <div className="flex h-3/5">
+                <div className="py-10 px-10 w-4/5 h-1/2">
+                    <img alt="shantanu" src="shantanu.png"></img>
+                </div>
+                <div className="py-10">
+                    <div className="flex items-baseline px-30">
+                        <p className="text-white font-bold text-6xl px-5">Hi!</p>
+                        <img alt="wavinghand" className="w-16 h-16 animate-bounce" src="hi_emoji.png"></img>
+                    </div>
+                    <div className="py-15">
+                        <p className="text-white font-bold text-5xl py-40 px-5">I'm Shantanu, an eternally curious, seldom lazy life form </p>
+                    </div>
+                </div>
+            </div>
+            <div className="h-1/5 text-white font-bold text-2xl px-20 py-20">
+                <p>Passionate about tech, sports and random obscure facts</p>
+                <p>Shape shifting from one form into another</p>
             </div>
         </div>
     );
