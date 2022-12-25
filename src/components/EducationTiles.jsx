e076ed85b7ffbd90549bb587e8d38905d9cc6c09
shantanugodbole <shantanugodbole2000@gmail.com>
2022-12-25 10:48:16 +0530

    designing academics

---
+++ src/components/EducationTiles.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EducationTiles(){
+    return(
+        <div className="bg-neutral-800 rounded-lg w-1/5">
+            Hello
+        </div>
+    )
+}
+
+export default EducationTiles;
--- src/components/content/Academics.jsx
+++ src/components/content/Academics.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import EducationTiles from "../EducationTiles";
+
 
 function Academics(){
     return (
-        <div>Academics</div>
+        <div className="w-5/6 bg-neutral-900">
+            <div className="px-10 py-10">
+                <p className="text-white font-bold text-2xl">Medley of my Formative Years</p>
+            </div>
+            <div className="flex w-full justify-evenly h-2/5">
+                <EducationTiles></EducationTiles>
+                <EducationTiles></EducationTiles>
+            </div>
+            <div className="flex w-full justify-center mt-5 h-2/5">
+                <EducationTiles></EducationTiles>
+            </div>
+        </div>
     );
 }
 
