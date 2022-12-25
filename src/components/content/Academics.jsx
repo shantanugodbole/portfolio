1f8da7966a97d31abee5a45d5883561596e241b2
shantanugodbole <shantanugodbole2000@gmail.com>
2022-12-25 14:29:59 +0530

    academics v1

--- src/components/content/Academics.jsx
+++ src/components/content/Academics.jsx
@@ -3,17 +3,27 @@ import EducationTiles from "../EducationTiles";
 
 
 function Academics(){
+    function handleClick(){
+        window.open("https://drive.google.com/file/d/1POwxUvtuTYEdN-HrZ9nAzf2F0fF2wJXM/view")
+    }
+
     return (
         <div className="w-5/6 bg-neutral-900">
-            <div className="px-10 py-10">
+            <div className="px-10 py-10 flex justify-between">
                 <p className="text-white font-bold text-2xl">Medley of my Formative Years</p>
+                <div className="group">
+                {/* <p className="opacity-0 group-hover:opacity-100 text-white font-italics">My Resume!</p> */}
+                <button onClick={handleClick} className="bg-indigo-700 px-2 py-2 rounded-full hover:animate-spin group-hover:opacity-50">
+                    <img src="resume.png" alt="resume"></img>
+                </button>
+                </div>
             </div>
             <div className="flex w-full justify-evenly h-2/5">
-                <EducationTiles></EducationTiles>
-                <EducationTiles></EducationTiles>
+                <EducationTiles img="balmohan.jpeg" title="Balmohan Vidyamandir" description="SSC:96%"></EducationTiles>
+                <EducationTiles img="ruia.jpg" title="Ruia Junior College" description="HSC:91%"></EducationTiles>
             </div>
             <div className="flex w-full justify-center mt-5 h-2/5">
-                <EducationTiles></EducationTiles>
+                <EducationTiles img="kjsce-logo.webp" title="K.J. Somaiya College of Engineering" description="GPA:9.45"></EducationTiles>
             </div>
         </div>
     );
