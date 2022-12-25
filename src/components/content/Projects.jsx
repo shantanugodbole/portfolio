c7999878faf85e4dad675b45d0b81c3a9a613ab1
shantanugodbole <shantanugodbole2000@gmail.com>
2022-12-25 17:32:14 +0530

    projects v2

--- src/components/content/Projects.jsx
+++ src/components/content/Projects.jsx
@@ -8,14 +8,14 @@ function Projects() {
                 <p className="text-white">My Mixtapes</p>
             </div>
                 <div className="flex justify-evenly h-1/3">
-                <ProjectTiles name="Crop Disease Detection"></ProjectTiles>
-                <ProjectTiles name="RIP-Proxy"></ProjectTiles>
-                <ProjectTiles name="Smart Health Assistant"></ProjectTiles>
+                <ProjectTiles name="Crop Disease Detection" description="Developed as part of a research internship"></ProjectTiles>
+                <ProjectTiles name="RIP-Proxy" description="A Flutter app designed to use Face Detection to mark student presence"></ProjectTiles>
+                <ProjectTiles name="Smart Health Assistant" description="A mobile app bundled with a ML predictor to help diagnosis"></ProjectTiles>
             </div>
                 <div className="flex justify-evenly h-1/3 mt-10">
-                    <ProjectTiles name="Ghar se Ghumo"></ProjectTiles>
-                    <ProjectTiles name="ShanListens"></ProjectTiles>
-                    <ProjectTiles name="Aapka Dost"></ProjectTiles>
+                    <ProjectTiles name="Ghar se Ghumo" description="An Augmented Reality application to aid people trapped at home during the pandemic"></ProjectTiles>
+                    <ProjectTiles name="ShanListens" description="A Twitter bot to tweet out the songs I'm currently listening to"></ProjectTiles>
+                    <ProjectTiles name="Aapka Dost" description="A smart CLI tool to disseminate FAQs about Covid-19"></ProjectTiles>
                 </div>
 
 
