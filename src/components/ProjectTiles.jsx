34471b215f50f242282c1c488d67dec3229a8ec1
shantanugodbole <shantanugodbole2000@gmail.com>
2022-12-26 11:45:16 +0530

    projects v4

--- src/components/ProjectTiles.jsx
+++ src/components/ProjectTiles.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 function ProjectTiles(props) {
+
+    let stack = props.stack
+    console.log(stack)
+
+    function handleClick(){
+        window.open(props.url)
+    }
     return (
-        <div className="bg-neutral-800 rounded-lg hover:bg-neutral-700 w-1/6">
-            <div className="rounded-lg">
+        <div className="bg-neutral-800 rounded-lg hover:bg-neutral-700 w-1/6" onClick={handleClick}>
+            <div className="rounded-lg m-auto">
                 <img src={props.img} alt="placeholder" className="h-1/3 w-full"></img>
             </div>
             <div className="py-2 px-2">
@@ -13,8 +20,8 @@ function ProjectTiles(props) {
                 <div className="py-2 text-white">
                     {props.description}
                 </div>
-                <div className="">
-                    <img src="flutter.png" alt="icon" className="h-7 w-7"></img>
+                <div className="pt-2 flex">
+                    {stack.map((tech) => (<img src={tech} alt="icon" className="h-7 w-7"></img>))}
                 </div>
             </div>
         </div>
--- src/components/content/Projects.jsx
+++ src/components/content/Projects.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import ProjectTiles from "../ProjectTiles";
 
 function Projects() {
+    const cdd_list = ['flutter.png', 'firebase.png', 'python.png'];
+    const rip_list = ['flutter.png', 'firebase.png', 'python.png'];
+    const sha_list = ['flutter.png', 'firebase.png', 'python.png'];
+    const gsglist = ['flutter.png', 'blender.png'];
+    const sglistenslist = ['python.png'];
+    const aapkadostlist = ['python.png'];
+    let cdd_url = "https://github.com/The-AlphaDevs/Crop-Disease-Detector-App"
     return (
         <div className="w-5/6 bg-neutral-900">
             <div className="font-bold px-10 py-10 text-2xl mb-10">
                 <p className="text-white">My Mixtapes</p>
             </div>
                 <div className="flex justify-evenly h-1/3">
-                <ProjectTiles img="cdd.jpeg" name="Crop Disease Detection" description="Developed as part of a research internship"></ProjectTiles>
-                <ProjectTiles img="rip-proxy.png" name="RIP-Proxy" description="A Flutter app leveraging face detection"></ProjectTiles>
-                <ProjectTiles img="sha.png" name="Smart Health Assistant" description="A mobile app bundled with a ML diagnoser"></ProjectTiles>
+                <ProjectTiles img="cdd.jpeg" name="Crop Disease Detection" description="Developed as part of a research internship" stack={cdd_list} url={cdd_url}></ProjectTiles>
+                <ProjectTiles img="rip-proxy.png" name="RIP-Proxy" description="A Flutter app leveraging face detection" stack={rip_list}></ProjectTiles>
+                <ProjectTiles img="sha.png" name="Smart Health Assistant" description="A mobile app bundled with a ML diagnoser" stack={sha_list}></ProjectTiles>
             </div>
                 <div className="flex justify-evenly h-1/3 mt-10">
-                    <ProjectTiles img="gharseghumo.jpeg" name="Ghar se Ghumo" description="Using AR to bring sights to your home."></ProjectTiles>
-                    <ProjectTiles img="spotify.png" name="ShanListens" description="A Twitter bot to tweet out the songs I'm currently listening to"></ProjectTiles>
-                    <ProjectTiles img="aapka dost.jpeg" name="Aapka Dost" description="Bot to answer Covid-19 FAQs"></ProjectTiles>
+                    <ProjectTiles img="gharseghumo.jpeg" name="Ghar se Ghumo" description="Using AR to bring sights to your home." stack={gsglist}></ProjectTiles>
+                    <ProjectTiles img="spotify.png" name="ShanListens" description="A Twitter bot to tweet out the songs I'm currently listening to" stack={sglistenslist}></ProjectTiles>
+                    <ProjectTiles img="aapka dost.jpeg" name="Aapka Dost" description="Bot to answer Covid-19 FAQs" stack={aapkadostlist}></ProjectTiles>
                 </div>
 
 
