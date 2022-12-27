b9af30938882ca2424d103a2927062b2fc86edad
shantanugodbole <shantanugodbole2000@gmail.com>
2022-12-27 21:21:41 +0530

    skills v2

--- src/components/content/Interests.jsx
+++ src/components/content/Interests.jsx
@@ -7,15 +7,35 @@ function Interests(){
                 <p className="text-white">Studio Sessions</p>
             </div>
             <div className="px-10 h-1/4">
-                <p>Tools and Technologies</p>
-                <div>
-                    Proficient:
+                <p className="text-white text-lg font-bold">Tools and Technologies</p>
+                <div className="flex flex-row px-2">
+                    <p className="text-white font-bold text-xl">Proficient:</p>
+                    <div className="flex flex-row space-x-3 px-2">
+                        <img src="flutter.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="python.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="firebase.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="gcp.png" alt="Icon" className="w-15 h-10"></img>
+                        <img src="django.png" alt="Icon" className="w-15 h-10"></img>
+                    </div>
                 </div>
-                <div>
-                    Intermediate:
+                <div className="flex flex-row px-2">
+                <p className="text-white font-bold text-xl">Intermediate:</p>
+                    <div className="flex flex-row space-x-3 px-2">
+                        <img src="sf.webp" alt="Icon" className="w-10 h-10"></img>
+                        <img src="logo512.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="tailwindcss.png" alt="Icon" className="w-15 h-10"></img>
+                        <img src="java.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="javascript.png" alt="Icon" className="w-10 h-10"></img>
+                    </div>
                 </div>
-                <div>
-                    Beginner:
+                <div className="flex flex-row px-2">
+                <p className="text-white font-bold text-xl">Beginner:</p>
+                    <div className="flex flex-row space-x-3 px-2">
+                        <img src="vue.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="nodejs.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="grapql.png" alt="Icon" className="w-10 h-10"></img>
+                        <img src="go.png" alt="Icon" className="w-10 h-10"></img>
+                    </div>
                 </div>
             </div>
             <div className="px-10 h-1/4">
